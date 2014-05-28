5960b34350934b3ae98772cf509853ee3b666b51
= <imcdmedia@Admins-MacBook-Pro.local>
2014-05-28 09:17:08 -0700

    remove css from sidebarWidget.js

--- sidebarWidget.js
+++ sidebarWidget.js
@@ -17,21 +17,21 @@
     }
 
     Plugin.prototype = {
-
         init: function() {
         	var $this = this;
         	var id = this.element.attr("id");
         	this.wrapper = this._generateWrapper(this.element);
         	this.pushContainer = this._getPushContainer(this.element);
 
-        	this._createTemporaryCss(); //see if it makes sense to move to stylesheet
-
         	$(document).on("click", "[sidebarWidget='"+id+"']", function(){
-        		$this._openSidebar();
+        		$this.openSidebar();
         	});
 
         	//move element to the left a little
         	this.element.css("left", this.pushContainer.offset().left - this.element.width());
+        },
+        openSidebar:function(){
+        	this.wrapper.css({left:0});
         },
         _generateWrapper: function(element){
         	var wrapper = element.children(".sidebarWrapper:first");
@@ -45,13 +45,6 @@
         },
         _getPushContainer:function(element){
         	return $(element.attr("pushContainer"));
-        },
-        _openSidebar:function(){
-        	this.wrapper.css({left:0});
-        },
-        _createTemporaryCss: function(el, options) {
-        	this.element.css({height:"100%", border:"1px solid black", width:150, position:"absolute", overflow:"hidden"});
-            this.wrapper.css({position:"absolute", left:"100%"});
         }
     };
 
